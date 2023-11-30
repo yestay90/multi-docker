fc13aae9a1d0a6cb72bdbf9ef4995fe135fd0e53
yestay90 <yestay90@gmail.com>
2023-11-30 14:16:28 +0600

    added client folder

---
+++ client/src/OtherPage.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const OtherPage = () => {
+  return (
+    <div>
+      Im some other page
+      <Link to="/"> Go back </Link>
+    </div>
+  );
+};
+
+export default OtherPage;
