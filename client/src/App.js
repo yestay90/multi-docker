a5b195258ee39fabbf9aa06b9c9b7b91c294c701
yestay90 <yestay90@gmail.com>
2023-12-01 17:52:25 +0600

    added title

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h1>Fib calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
           <div>
